af99e873b0c8062dac1a164bd3be22d87b7c0841
Kantor Alexandru <44375270+kalex411@users.noreply.github.com>
2018-11-14 20:16:53 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# workshop_js1
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,14 @@
+/* alert('alert from JS file') */
+// number variables
+var number = 17;
+console.log("Number=", number);
+// string variables
+var name = "Kantor";
+console.log("Name=", name);
+// boolean variables
+var isTrue = true;
+console.log("Is true=", isTrue);
+var isFalse = false;
+console.log("Is false=", isFalse);
+// + operator
+console.log("sum=", 2 + 2);
